25b2a52d1c4dc9faa7aa20a8fc4f8fc6c0c8b9e2
Amlal <amlal@nekernel.org>
2026-04-16 11:25:41 +0200

    [FEAT] src: Servlet architecture.
    Signed-off-by: Amlal <amlal@nekernel.org>

--- src/system/IServletWorker.java
+++ src/system/IServletWorker.java
@@ -1,6 +1,10 @@
+/**
+ * Copyright - Ne.app
+ */
 package system;
 
+/// @brief A servlet worker interface to run and stop servlets
 public interface IServletWorker {
     public IServlet startServlet();
-    public IServlet stopServlet();
+    public void stopServlet(IServlet);
 }
